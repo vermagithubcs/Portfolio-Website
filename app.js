8196d23c84213ceac289a7cff57bc4c52583d799
vermagithubcs <vermadigital943@gmail.com>
2024-04-21 16:14:18 +0530

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// Portfolio link to direct to new page
-let btnButton = document.getElementById('btn');
-btnButton.addEventListener("click", () => {
-    window.location.href = 'portfolio.html';
-})
 // home link to direct to the about page
 let btnBig = document.getElementById('btnMore');
 btnBig.addEventListener("click", () => {
